78dc03a1f83710c99efad2113d21972163abc0fc
SaltyEgg <hzhou.me@saltyee.com>
2024-07-04 14:58:30 -0700

    clean up and add xata db

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import renderHtml from "./renderHtml.js";
 var src_default = {
   async fetch(request, env) {
     const { DATABASE } = env;
-    const stmt = DATABASE.prepare("SELECT * FROM comments LIMIT 3");
+    const stmt = DATABASE.prepare("SELECT * FROM comments LIMIT 2");
     const { results } = await stmt.all();
     return new Response(
       renderHtml(JSON.stringify(results, null, 2)),
