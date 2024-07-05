120a764986f4871a91f71f4eeac78eeb0c66c0aa
SaltyEgg <hzhou.me@saltyee.com>
2024-07-04 18:50:10 -0700

    simple hello world

--- src/index.js
+++ src/index.js
@@ -1,20 +1,7 @@
-// src/templates/populated-worker/src/index.js
-import renderHtml from "./renderHtml.js";
-var src_default = {
-  async fetch(request, env) {
-    const { DATABASE } = env;
-    const stmt = DATABASE.prepare("SELECT * FROM comments LIMIT 2");
-    const { results } = await stmt.all();
-    return new Response(
-      renderHtml(JSON.stringify(results, null, 2)),
-      {
-        headers: {
-          "content-type": "text/html"
-        }
-      }
-    );
-  }
-};
-export {
-  src_default as default
-};
+import FastifyEdge from 'fastify-edge';
+
+const app = FastifyEdge();
+
+app.get('/', (_, reply) => {
+	reply.send({ 'Hello': 'World' });
+});
