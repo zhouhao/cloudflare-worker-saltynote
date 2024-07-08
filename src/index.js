c3f274fe1207b097472c5486abd292e40d4c6331
SaltyEgg <hzhou.me@saltyee.com>
2024-07-08 00:41:36 -0700

    Revert "clean up and add xata db"
    This reverts commit 78dc03a1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
+// src/templates/populated-worker/src/index.js
+import renderHtml from "./renderHtml.js";
 import FastifyEdge from 'fastify-edge';
+
 
 const app = FastifyEdge();
 
 app.get('/', (_, reply) => {
 	reply.send({ 'Hello': 'World' });
 });
+
+
+var src_default = {
+  async fetch(request, env) {
+    const { DATABASE } = env;
+    const stmt = DATABASE.prepare("SELECT * FROM comments LIMIT 3");
+    const { results } = await stmt.all();
+    return new Response(
+      renderHtml(JSON.stringify(results, null, 2)),
+      {
+        headers: {
+          "content-type": "text/html"
+        }
+      }
+    );
+  }
+};
+export {
+  src_default as default
+};
+
+
