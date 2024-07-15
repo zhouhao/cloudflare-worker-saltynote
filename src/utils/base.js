43b62a80f3a2d67733a4e2065298f93b38a9f10a
SaltyEgg <hzhou.me@saltyee.com>
2024-07-15 11:40:27 -0700

    add utils for return token pair

--- src/utils/base.js
+++ src/utils/base.js
@@ -1,6 +1,23 @@
+import { sign } from 'hono/jwt';
+
 // balance the numbers and the letters
 const characters = '0123456789ABCDEFGHIJKLMN0123456789OPQRSTUVWXYZ0123456789';
 
 export const generateCode = (length) => {
   return Array.from({ length }, () => characters.charAt(Math.floor(Math.random() * characters.length))).join('');
+};
+
+export const generateTokenPair = async (user, env) => {
+  return {
+    'access_token': await sign({
+      sub: user.id,
+      email: user.email,
+      exp: Math.floor(Date.now() / 1000) + 60 * 60 // Token expires in 1 hour
+    }, env.JWT_ACCESS_SECRET),
+    'refresh_token': await sign({
+      sub: user.id,
+      email: user.email,
+      exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 90 // Token expires in 90 days
+    }, env.JWT_REFRESH_SECRET)
+  };
 };
