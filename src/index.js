03da77081f9e9d61a81f9b3a5cd91118bb1f798c
SaltyEgg <hzhou.me@saltyee.com>
2024-07-15 01:15:02 -0700

    enable kv binding and demo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
 import { Hono } from 'hono';
 import { sign, jwt } from 'hono/jwt';
+import { sendEmail } from './handler/email';
+import { generateCode } from './utils/base';
+import { kvGet, saveEmailVerifyCode } from './persist/kv-store';
 
 
 const app = new Hono();
+
+// ---- User API ----
+
+// 1. send email verify code
+app.post('/email', async c => {
+  const { email } = await c.req.json();
+  const verifyCode = generateCode(8);
+  await saveEmailVerifyCode(c.env.saltynote, email, verifyCode);
+  await sendEmail(email, generateCode(8), c.env.RESEND_API_KEY);
+  return c.json({ 'message': await kvGet(c.env.KV, 'hello') + 'Email verification code has been sent.' + await kvGet(c.env.KV, email) });
+});
+
+// 2. login with email verify code
+
+app.post('/login', c => {
+
+});
+
+
+// ---- Page Annotation API ----
+
 
 const secret = 'it-is-very-secret';
 
